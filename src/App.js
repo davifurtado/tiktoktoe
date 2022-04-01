3cea5b34b20fb789c7d0d0f6d5e6b0754feff33a
Davi Furtado <davi.bmfurtado@gmail.com>
2022-04-01 14:52:38 -0300

    created basic state for the board and stylized squares for board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useState } from 'react';
+import Square from './components/Square';
+
 import './App.css';
 
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+
+
   return (
     <div className="App">
       <div className="board">
-        <div className="boardRow"></div>
-        <div className="boardRow"></div>
-        <div className="boardRow"></div>
+        <div className="boardRow">
+          <Square val={board[0] || ""} />
+          <Square val={board[1] || ""} />
+          <Square val={board[2] || ""} />
+        </div>
+        <div className="boardRow">
+          <Square val={board[0] || ""} />
+          <Square val={board[1] || ""} />
+          <Square val={board[2] || ""} />
+        </div>
+        <div className="boardRow">
+          <Square val={board[0] || ""} />
+          <Square val={board[1] || ""} />
+          <Square val={board[2] || ""} />
+        </div>
       </div>
     </div>
   );
