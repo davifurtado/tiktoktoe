bd20fc422fb4bbebebc77af512e5aa7a3157bd4e
Davi Furtado <davi.bmfurtado@gmail.com>
2022-04-01 15:11:56 -0300

    created basic logic to select squares and to change players

--- src/App.js
+++ src/App.js
@@ -5,25 +5,68 @@ import './App.css';
 
 function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [playerOne, setPlayerOne] = useState("X")
+  const [playerTwo, setPlayerTwo] = useState("O")
+  const [currentPlayer, setCurrentPlayer] = useState(null)
 
+  const onClickSquare = (squareIndex) => {
+    const newBoard = [...board]
+    if (newBoard[squareIndex]) return false;
+
+    newBoard[squareIndex] = currentPlayer || playerOne
+    setBoard(newBoard)
+    if (currentPlayer === playerOne || !currentPlayer) {
+      setCurrentPlayer(playerTwo)
+    } else {
+      setCurrentPlayer(playerOne)
+    }
+    
+  }
 
   return (
     <div className="App">
       <div className="board">
         <div className="boardRow">
-          <Square value={board[0] || ""} />
-          <Square value={board[1] || ""} />
-          <Square value={board[2] || ""} />
+          <Square 
+            value={board[0] || ""} 
+            onClickSquare={() => onClickSquare(0)}
+          />
+          <Square 
+            value={board[1] || ""} 
+            onClickSquare={() => onClickSquare(1)}
+          />
+          <Square 
+            value={board[2] || ""} 
+            onClickSquare={() => onClickSquare(2)}
+          />
         </div>
         <div className="boardRow">
-          <Square value={board[0] || ""} />
-          <Square value={board[1] || ""} />
-          <Square value={board[2] || ""} />
+          <Square 
+            value={board[3] || ""} 
+            onClickSquare={() => onClickSquare(3)}
+          />
+          <Square 
+            value={board[4] || ""} 
+            onClickSquare={() => onClickSquare(4)}
+          />
+          <Square 
+            value={board[5] || ""} 
+            onClickSquare={() => onClickSquare(5)}
+          />
         </div>
         <div className="boardRow">
-          <Square value={board[0] || ""} />
-          <Square value={board[1] || ""} />
-          <Square value={board[2] || ""} />
+          <Square 
+            value={board[6] || ""} 
+            onClickSquare={() => onClickSquare(6)}
+          />
+          <Square 
+            value={board[7] || ""} 
+            onClickSquare={() => onClickSquare(7)}
+          />
+          <Square 
+            value={board[8] || ""} 
+            onClickSquare={() => onClickSquare(8)}
+          />
         </div>
       </div>
     </div>
